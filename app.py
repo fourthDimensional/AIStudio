fdc9434d3f26ee7ec2d729c236a78655b33ffe64
Sam <finepointdog@gmail.com>
2023-06-25 13:21:07 -0500

    added dataset upload/deletion

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
+import data_proc as dp
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 300 + (1024 * 1024) # Basic request size + large dataset limit
 
 def convert_text_file_to_list(file_path):
     with open(file_path, 'r') as file:
@@ -12,28 +15,59 @@ def convert_text_file_to_list(file_path):
     return lines
 text_file_path = 'apikeys.txt'
 api_keys = convert_text_file_to_list(text_file_path)
+print(api_keys)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/add', methods=['GET'])
-def add():
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+
+
+
+@app.route('/data/upload', methods=['POST'])
+def upload():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
         return {'error': 'Invalid API Key'}, 401
-    param1 = request.args.get('one')
-    param2 = request.args.get('two')   
-    return jsonify(int(param1) + int(param2)), 200
+    
+    uploaded_file = request.files['file']
+    id = request.form.get('id')
+    
+    if id == None:
+        return {'error': 'No Dataset ID provided'}, 400
+    
+    if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}".format(id, api_key))):
+        return {'error': 'Dataset already exists; delete existing set before trying again'}, 409
+    
+    if uploaded_file.filename != '':
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}".format(id, api_key))
+        uploaded_file.save(file_path)
+        return {'info': 'Dataset uploaded'}, 200
+    return {'error': 'Dataset not uploaded or given a file name'}, 412
 
-@app.route('/add/three', methods=['GET'])
-def addthree():
+
+
+@app.route('/data/delete', methods=['DELETE'])
+def deleteData():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
         return {'error': 'Invalid API Key'}, 401
-    param1 = request.args.get('one')
-    return jsonify(int(param1) + 3), 200
+    
+    id = request.form.get('id')
+    
+    if id == None or id == '': 
+        return {'error': 'No Dataset ID provided'}, 400
+    
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}".format(id, api_key))
+    
+    if os.path.exists(file_path) == False:
+        return {'error': 'Dataset does not exist; create one before trying again'}, 409
+    
+    os.remove(file_path)
+    return {'info': 'Dataset deleted'}, 200
 
 
-
+ 
 if __name__ == '__main__':
     app.run(port=5001)
