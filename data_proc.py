e0bb833781a9a18fe664704f051a2dcb9cd5736e
Sam <finepointdog@gmail.com>
2023-06-29 14:04:49 -0500

    Modified data_proc.py

--- data_proc.py
+++ data_proc.py
@@ -8,7 +8,70 @@ logging.basicConfig(format='%(levelname)s (%(asctime)s): %(message)s (Line: %(li
 
 
 class Preprocessor_Layer:
-    def create_layer(self):
+    def __init__(self, layer_id):
+        self.layer_id = layer_id
+        self.type = ""  # Numerical, Text, Categorical
+
+    def create_instanced_layer(self):
         raise NotImplementedError
 
 
+# TODO add text vectorization
+class Normalization_Layer(Preprocessor_Layer):
+    def __init__(self, layer_id):
+        super().__init__(layer_id)
+        self.layer_id = layer_id
+        self.type = "numerical"
+
+    def create_instanced_layer(self):
+        pass
+
+
+class Discretization_Layer(Preprocessor_Layer):
+    def __init__(self, layer_id):
+        super().__init__(layer_id)
+        self.layer_id = layer_id
+        self.type = "numerical"
+
+    def create_instanced_layer(self):
+        pass
+
+
+class Category_Encoding_Layer(Preprocessor_Layer):
+    def __init__(self, layer_id):
+        super().__init__(layer_id)
+        self.layer_id = layer_id
+        self.type = "numerical"
+
+    def create_instanced_layer(self):
+        pass
+
+
+class Hashing_Layer(Preprocessor_Layer):
+    def __init__(self, layer_id):
+        super().__init__(layer_id)
+        self.layer_id = layer_id
+        self.type = "numerical"
+
+    def create_instanced_layer(self):
+        pass
+
+
+class String_Lookup_Layer(Preprocessor_Layer):
+    def __init__(self, layer_id):
+        super().__init__(layer_id)
+        self.layer_id = layer_id
+        self.type = "numerical"
+
+    def create_instanced_layer(self):
+        pass
+
+
+class Integer_Lookup_Layer(Preprocessor_Layer):
+    def __init__(self, layer_id):
+        super().__init__(layer_id)
+        self.layer_id = layer_id
+        self.type = "numerical"
+
+    def create_instanced_layer(self):
+        pass
