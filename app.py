4adb4f8d58a8993196519aa1693821f4fbb116a0
Sam <finepointdog@gmail.com>
2023-07-11 15:42:59 -0500

    Reformated and refactored things 2

--- app.py
+++ app.py
@@ -19,12 +19,23 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 UPLOAD_FOLDER = 'static/files'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+REQUEST_SUCCEEDED = 200
+REQUEST_CREATED = 201
+
+BAD_REQUEST = 400
+UNAUTHENTICATED_REQUEST = 401
+FORBIDDEN_REQUEST = 403
+PAGE_NOT_FOUND = 404
+REQUEST_CONFLICT = 409
+
+REQUEST_NOT_IMPLEMENTED = 501
+
 
 @app.route('/data/upload', methods=['POST'])
 def upload():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     uploaded_file = request.files['file']
 
@@ -33,49 +44,49 @@ def upload():
     given_id = request.form.get('id')
 
     if given_id is None:
-        return {'error': 'No Dataset ID provided'}, 400
+        return {'error': 'No Dataset ID provided'}, BAD_REQUEST
 
     if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}.csv".format(given_id, api_key))):
-        return {'error': 'Dataset already exists; delete existing set before trying again'}, 409
+        return {'error': 'Dataset already exists; delete existing set before trying again'}, REQUEST_CONFLICT
 
     if uploaded_file.filename != '':
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}.csv".format(given_id, api_key))
         uploaded_file.save(file_path)
-        return {'info': 'Dataset uploaded'}, 200
+        return {'info': 'Dataset uploaded'}, REQUEST_SUCCEEDED
 
-    return {'error': 'Dataset not uploaded or given a file name'}, 412
+    return {'error': 'Dataset not uploaded or given a file name'}, BAD_REQUEST
 
 
 @app.route('/data/delete', methods=['DELETE'])
 def delete_data():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     given_id = request.form.get('id')
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}.csv".format(given_id, api_key))
 
     if given_id is None or given_id == '':
-        return {'error': 'No Dataset ID provided'}, 400
+        return {'error': 'No Dataset ID provided'}, BAD_REQUEST
 
     if not os.path.exists(file_path):
-        return {'error': 'Dataset does not exist; create one before trying again'}, 409
+        return {'error': 'Dataset does not exist; create one before trying again'}, REQUEST_CONFLICT
 
     os.remove(file_path)
-    return {'info': 'Dataset deleted'}, 200
+    return {'info': 'Dataset deleted'}, REQUEST_SUCCEEDED
 
 
 @app.route('/model/create', methods=['POST'])
 def create_model():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     given_id = request.form.get('id')
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], "dataset_{}_{}.csv".format(given_id, api_key))
 
     if not os.path.exists(file_path):
-        return {'error': 'Dataset does not exist; create one before trying again'}, 409
+        return {'error': 'Dataset does not exist; create one before trying again'}, REQUEST_CONFLICT
 
     model_name = request.form.get('model_name')
     given_type = request.form.get('type')
@@ -89,22 +100,22 @@ def create_model():
 
     utils.save(model, model_path)
 
-    return return_value
+    return return_value, REQUEST_CREATED
 
 
 @app.route('/model/name', methods=['GET'])
 def get_model_name():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     given_id = request.form.get('id')
 
     if given_id is None or given_id == '':
-        return {'error': 'No Dataset ID provided'}, 400
+        return {'error': 'No Dataset ID provided'}, BAD_REQUEST
 
     if not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], "model_{}_{}.pk1".format(given_id, api_key))):
-        return {'error': 'Model does not exist; create one before trying again'}, 409
+        return {'error': 'Model does not exist; create one before trying again'}, REQUEST_CONFLICT
 
     model = utils.load_model_from_file(given_id, api_key, app.config['UPLOAD_FOLDER'])
 
@@ -115,7 +126,7 @@ def get_model_name():
 def get_columns():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     # TODO Add id checking
 
@@ -123,14 +134,14 @@ def get_columns():
 
     model = utils.load_model_from_file(given_id, api_key, app.config['UPLOAD_FOLDER'])
 
-    return model.process_columns(process_modifications=True), 200
+    return model.process_columns(process_modifications=True), REQUEST_SUCCEEDED
 
 
 @app.route('/model/columns/', methods=['DELETE'])
 def add_column_deletion():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     given_id = request.form.get('id')
     given_column = request.form.get('column')
@@ -140,23 +151,23 @@ def add_column_deletion():
     model = utils.load_model_from_file(given_id, api_key, app.config['UPLOAD_FOLDER'])
 
     if model.data_modification_exists(data_proc.Column_Deletion, given_column):
-        return {'error': 'Column Deletion already added'}, 400
+        return {'error': 'Column Deletion already added'}, REQUEST_CONFLICT
 
     if given_column not in model.process_columns(process_modifications=False):
-        return {'error': 'Given column does not exist'}, 400
+        return {'error': 'Given column does not exist'}, BAD_REQUEST
 
     model.delete_column(str(given_column))
 
     utils.save(model, model.model_path)
 
-    return {'info': 'Column Deletion added'}, 200
+    return {'info': 'Column Deletion added'}, REQUEST_CREATED
 
 
 @app.route('/model/columns/', methods=['POST'])
 def undo_column_deletion():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     given_id = request.form.get('id')
     given_column = request.form.get('column')
@@ -166,26 +177,42 @@ def undo_column_deletion():
     model = utils.load_model_from_file(given_id, api_key, app.config['UPLOAD_FOLDER'])
 
     if not model.data_modification_exists(data_proc.Column_Deletion, given_column):
-        return {'error': 'Column Deletion does not exist'}, 400
+        return {'error': 'Column Deletion does not exist'}, BAD_REQUEST
 
     model.add_deleted_column(given_column)
 
     utils.save(model, model.model_path)
 
-    return {'info': 'Column Deletion removed'}, 200
+    return {'info': 'Column Deletion removed'}, REQUEST_SUCCEEDED
 
 
 @app.route('/model/preprocessing/create', methods=['POST'])
 def create_preprocessing_layer():
     api_key = request.headers.get('API-Key')
     if api_key not in api_keys:
-        return {'error': 'Invalid API Key'}, 401
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
 
     given_id = request.form.get('id')
 
     # TODO Add id checking
 
     model = utils.load_model_from_file(given_id, api_key, app.config['UPLOAD_FOLDER'])
+
+
+@app.route('/model/preprocessing/delete', methods=['DELETE'])
+def delete_preprocessing_layer():
+    api_key = request.headers.get('API-Key')
+    if api_key not in api_keys:
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
+    
+    
+@app.route('/', methods=[])
+def ():
+    api_key = request.headers.get('API-Key')
+    if api_key not in api_keys:
+        return {'error': 'Invalid API Key'}, UNAUTHENTICATED_REQUEST
+    
+
 
 
 if __name__ == '__main__':
