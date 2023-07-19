8bc7e99002979ec94b6cf3127daec7c521e30800
Sam <finepointdog@gmail.com>
2023-07-18 22:11:30 -0500

    Implemented abstract base classes to the layer class to increase code clarity.
    Signed-off-by: Sam <finepointdog@gmail.com>

--- routes/helpers/layers.py
+++ routes/helpers/layers.py
@@ -1,27 +1,33 @@
 import tensorflow as tf
+from abc import ABC, abstractmethod
 
 
-class Layer:
+class Layer(ABC):
     def __init__(self, layer_id, input_type, input_shape, previous_layer):
         self.layer_id = layer_id
         self.input_type = input_type
         self.input_shape = input_shape
         self.previous_layer = previous_layer
 
-        self.next_layer = None
+        self.layer_map = {}
 
+    @abstractmethod
     def create_instanced_layer(self):
         pass
 
+    @abstractmethod
     def list_hyperparameters(self):
         pass
 
+    @abstractmethod
     def modify_hyperparameters(self):
         pass
 
+    @abstractmethod
     def get_default_hyperparameter(self):
         pass
 
+    @abstractmethod
     def suggested_hyperparameter(self):
         pass
 
