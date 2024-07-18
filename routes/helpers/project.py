164c70efb3c4c2e7483d5cf4dd39c7d9c4008aeb
Sam B <finepointdog@gmail.com>
2024-07-17 22:22:19 -0500

    Added project create, delete, and list routes

--- routes/helpers/project.py
+++ routes/helpers/project.py
@@ -13,13 +13,47 @@ Will be used to store and manage projects and their associated models and featur
 """
 
 class Project:
-    def __init__(self, dataset_key: str, model_compiler: ModelCompiler, training_config_packager: TrainingConfigPackager):
+    def __init__(self, dataset_key: str, model_compiler: ModelCompiler, training_config_packager: TrainingConfigPackager,
+                 title: str = None, description: str = None):
         self.model_registry: dict = {}
         self.feature_registry: dict = {}
 
-        self.dataset_key: str = dataset
-        self.project_key: str = hex(secrets.token_hex(4)) # Generate a random project key
+        self.title = title
+        self.description = description
+
+        self.dataset_key: str = dataset_key
+        self.project_key: str = str(secrets.token_hex(nbytes=4)) # Generate a random project key
 
         self.model_compiler: ModelCompiler = model_compiler
         self.training_config_packager: TrainingConfigPackager = training_config_packager
+
+    def serialize(self):
+        return {
+            's_ver': 1,
+            'title': self.title,
+            'description': self.description,
+            'dataset_key': self.dataset_key,
+            'project_key': self.project_key,
+            'models': {model_key: model.serialize() for model_key, model in self.model_registry.items()},
+            'features': {feature_key: feature.serialize() for feature_key, feature in self.feature_registry.items()}
+        }
+
+    @staticmethod
+    def deserialize(cls):
+        project = cls()
+
+        match cls['s_ver']:
+            case 1:
+                project.dataset_key = cls['dataset_key']
+                project.project_key = cls['project_key']
+                project.title = cls['title']
+                project.description = cls['description']
+                project.model_registry = {model_key: Model.deserialize(model) for model_key, model in cls['models'].items()}
+                project.feature_registry = {feature_key: Feature.deserialize(feature) for feature_key, feature in cls['features'].items()}
+                return project
+            case _:
+                logging.error('Unknown serialization version')
+                return None
+
+
 
