c003a002d4954fdb4f7f87392f19418a16f02086
Sam <61994039+fourthDimensional@users.noreply.github.com>
2024-11-24 15:51:07 -0600

    Added date feature extraction
    Took 27 minutes

--- routes/helpers/submodules/data_proc.py
+++ routes/helpers/submodules/data_proc.py
@@ -2,6 +2,7 @@ import logging
 from class_registry import ClassRegistry
 from keras.layers import StringLookup as KerasStringLookup
 import pandas as pd
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -65,25 +66,76 @@ class DataModification:
 data_mod_registry = ClassRegistry[DataModification]()
 
 
+# TODO Column deletion of date_day also deletes date_dayofyear because of the multi-column regex checking
 @data_mod_registry.register('column_deletion')
 class ColumnDeletion(DataModification):
-    def __init__(self, column_name):
-        self.column_name = column_name
+    def __init__(self, column_names):
+        self.column_names = column_names
 
     def apply(self, dataframe):
-        columns_to_drop = dataframe.filter(regex=f'^{self.column_name}').columns
+        columns_to_drop = []
+        for column_name in self.column_names:
+            columns_to_drop.extend(dataframe.filter(regex=f'^{column_name}').columns)
         return dataframe.drop(columns=columns_to_drop)
+
+    def __str__(self):
+        return ', '.join(self.column_names)
+
+
+@data_mod_registry.register('date_feature')
+class DateFeatureExtraction(DataModification):
+    def __init__(self, column_name):
+        self.column_name = column_name
+        self.date_patterns = [
+            re.compile(r'\d{4}-\d{2}-\d{2}'),  # YYYY-MM-DD
+            re.compile(r'\d{2}/\d{2}/\d{4}'),  # MM/DD/YYYY
+            re.compile(r'\d{2}-\d{2}-\d{4}'),  # DD-MM-YYYY
+        ]
+
+    def apply(self, dataframe):
+        def extract_date_features(date_str):
+            for pattern in self.date_patterns:
+                if pattern.match(date_str):
+                    date = pd.to_datetime(date_str, errors='coerce')
+                    return pd.Series({
+                        f'{self.column_name}_year': date.year,
+                        f'{self.column_name}_month': date.month,
+                        f'{self.column_name}_day': date.day,
+                        f'{self.column_name}_dy': date.dayofyear,
+                    })
+            return pd.Series({
+                f'{self.column_name}_year': None,
+                f'{self.column_name}_month': None,
+                f'{self.column_name}_day': None,
+                f'{self.column_name}_dy': None,
+            })
+
+        date_features = dataframe[self.column_name].apply(extract_date_features)
+
+        # def normalize(series):
+        #     min_val = series.min()
+        #     max_val = series.max()
+        #     if min_val == max_val:
+        #         return series.apply(lambda x: 0)
+        #     return (series - min_val) / (max_val - min_val)
+
+        # date_features = date_features.apply(normalize, axis=0)
+        dataframe = dataframe.drop(columns=[self.column_name])
+        dataframe = pd.concat([dataframe, date_features], axis=1)
+        return dataframe
 
     def __str__(self):
         return self.column_name
 
 
+@data_mod_registry.register('specified_feature')
 class StringLookup(DataModification):
     def __init__(self, column_name, lookup_map=None):
         self.column_name = column_name
         self.lookup_layer = KerasStringLookup(output_mode='one_hot', vocabulary=lookup_map)
 
     def adapt(self, data):
+        print(data)
         self.lookup_layer.adapt(data[self.column_name])
 
     def apply(self, data):
--- test_usage.py
+++ test_usage.py
@@ -2,7 +2,7 @@ from redis import Redis
 from routes.helpers.submodules.storage import RedisFileStorage
 from routes.helpers.submodules.storage import StorageInterface
 from routes.helpers.compiler import ModelCompiler
-from routes.helpers.jobs import JobConfigPackager
+import routes.helpers.jobs as jobs
 import routes.helpers.model as model
 import routes.helpers.submodules.layers_registry as layers
 import routes.helpers.submodules.data_proc as data_proc
@@ -28,6 +28,8 @@ REDIS_CONNECTION_INFO = {
     'decode_responses': True
 }
 
+jobs.kill_all_workers(Redis(**REDIS_CONNECTION_INFO))
+
 dataset_storage = StorageInterface(RedisFileStorage(Redis(**REDIS_CONNECTION_INFO)))
 
 api_key = '0e88f732d5f4d145130de7e210cd9a03'
@@ -45,45 +47,43 @@ layer_manipulator = model.LayerManipulator()
 hyperparameter_manager = model.HyperparameterManager()
 dataprocessing_engine = model.DataProcessingEngine()
 
-dataprocessing_engine.add_modification(data_proc.ColumnDeletion('date'))
+dataprocessing_engine.add_modification(data_proc.DateFeatureExtraction('date'))
 dataprocessing_engine.add_modification(data_proc.StringLookup('weather_condition'))
+
+# TODO Column deletion of date_day also deletes date_dayofyear because of the multi-column regex checking
+
+dataprocessing_engine.add_modification(data_proc.ColumnDeletion(['date_month', 'date_day', 'date_year']))
 dataprocessing_engine.set_input_fields(dataframe)
-dataprocessing_engine.add_label_column('rainfall')
+dataprocessing_engine.add_label_column('weather_condition')
 
 x, y = dataprocessing_engine.separate_labels(dataframe)
 
-input_layer = layers.InputLayer(input_size=6)
-dense_layer = layers.DenseLayer(units=5)
+pd.DataFrame(x).to_csv('x.csv')
+pd.DataFrame(y).to_csv('y.csv')
+
+input_layer = layers.InputLayer(input_size=5)
+dense_layer = layers.DenseLayer(units=10)
 
 layer_manipulator.add_layer(input_layer, 0, 0)
 layer_manipulator.forward_layer(0, 0)
 layer_manipulator.add_layer(layers.BatchNormalizationLayer(), 1, 0)
-# layer_manipulator.forward_layer(1, 0)
-# layer_manipulator.add_layer(dense_layer, 2, 0)
-# layer_manipulator.forward_layer(2, 0)
-# layer_manipulator.add_layer(dense_layer, 3, 0)
-# layer_manipulator.forward_layer(3, 0)
-# layer_manipulator.add_layer(layers.DenseLayer(units=1), 4, 0)
-layer_manipulator.point_layer(1, 0, 2, 0, 3)
-layer_manipulator.point_layer(1, 0, 2, 1, 3)
+layer_manipulator.forward_layer(1, 0)
 layer_manipulator.add_layer(dense_layer, 2, 0)
 layer_manipulator.forward_layer(2, 0)
-layer_manipulator.add_layer(dense_layer, 2, 1)
-layer_manipulator.point_layer(2, 1, 4, 0, 0)
 layer_manipulator.add_layer(dense_layer, 3, 0)
 layer_manipulator.forward_layer(3, 0)
-layer_manipulator.add_layer(dense_layer, 4, 0)
+layer_manipulator.add_layer(layers.DenseLayer(units=3), 4, 0)
 
 
 model_compiler = ModelCompiler()
-config_packager = JobConfigPackager()
+config_packager = jobs.JobConfigPackager()
 
 new_model = model.ModelWrapper(dataprocessing_engine, hyperparameter_manager, layer_manipulator)
 
 model = new_model.compile_model(model_compiler)
 
 model.compile(optimizer=Adam(), loss=MeanSquaredError(), metrics=[BinaryAccuracy()])
-model.fit(x, y, epochs=100, batch_size=64)
+model.fit(x, y, epochs=100, batch_size=11)
 
 print(model_compiler.input_storage)
 
