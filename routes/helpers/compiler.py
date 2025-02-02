99d9480e25590c3d14c255839f73252c5fab8b7a
Sam <61994039+fourthDimensional@users.noreply.github.com>
2025-02-01 23:52:34 -0600

    Reduced cognitive complexity of compiler.py's compile model function set.
    Took 14 minutes

--- routes/helpers/compiler.py
+++ routes/helpers/compiler.py
@@ -41,8 +41,8 @@ class ModelCompiler:
         input_layer = None
         previous_layer = 2 # don't change this, it breaks it.
 
-        for slice in layers:
-            input_layer, previous_layer = self._process_slice(layers[slice], input_layer, previous_layer)
+        for layer_slice in layers:
+            input_layer, previous_layer = self._process_slice(layers[layer_slice], input_layer, previous_layer)
             self.current_x += 1
             self.current_y = 0
 
@@ -68,58 +68,71 @@ class ModelCompiler:
 
     def _process_layer(self, layer_object, previous_layer):
         """Process a single layer and return its tensor."""
-        if isinstance(previous_layer, int):
-            layer_tensor = layer_object['layer'].instance_layer(previous_layer)
-        elif len(self.input_storage[self.current_x][self.current_y]) > 1:
-            concat_layer = keras.layers.Concatenate()(self.input_storage[self.current_x][self.current_y])
-            layer_tensor = layer_object['layer'].instance_layer(concat_layer)
-        else:
-            layer_tensor = layer_object['layer'].instance_layer(self.input_storage[self.current_x][self.current_y][0])
-
+        layer_tensor = self._get_layer_tensor(layer_object, previous_layer)
         outputs = layer_object['outputs']
 
         if outputs:
-            # Subsplit is present
-            if outputs[0][0] > 0:
-                subsplit_dimensions = self._get_subsplit_dimensions(outputs)
-                splitter = UniversalSplitLayer(num_or_size_splits=subsplit_dimensions, axis=-1)
-                split_tensors_list = splitter.split(layer_tensor)
-
-                index = 0
-                for output in outputs:
-                    positional_index = output[1]
-                    vertical_index = output[2]
-
-                    if positional_index not in self.input_storage:
-                        self.input_storage[positional_index] = {}
-
-                    if vertical_index not in self.input_storage[positional_index]:
-                        self.input_storage[positional_index][vertical_index] = []
-
-                    self.input_storage[positional_index][vertical_index].append(split_tensors_list[index])
-                    index += 1
-            else:
-                for output in outputs:
-                    positional_index = output[1]
-                    vertical_index = output[2]
-
-                    if positional_index not in self.input_storage:
-                        self.input_storage[positional_index] = {}
-
-                    if vertical_index not in self.input_storage[positional_index]:
-                        self.input_storage[positional_index][vertical_index] = []
-
-                    self.input_storage[positional_index][vertical_index].append(layer_tensor)
+            self._handle_outputs(layer_tensor, outputs)
         else:
             return layer_tensor
 
         return layer_tensor
 
+    def _get_layer_tensor(self, layer_object, previous_layer):
+        """Get the tensor for the current layer based on the previous layer."""
+        if isinstance(previous_layer, int):
+            return layer_object['layer'].instance_layer(previous_layer)
+        elif len(self.input_storage[self.current_x][self.current_y]) > 1:
+            concat_layer = keras.layers.Concatenate()(self.input_storage[self.current_x][self.current_y])
+            return layer_object['layer'].instance_layer(concat_layer)
+        else:
+            return layer_object['layer'].instance_layer(self.input_storage[self.current_x][self.current_y][0])
+
+    def _handle_outputs(self, layer_tensor, outputs):
+        """Handle the outputs of the current layer."""
+        if outputs[0][0] > 0:
+            self._handle_subsplit(layer_tensor, outputs)
+        else:
+            self._store_output(layer_tensor, outputs)
+
+    def _handle_subsplit(self, layer_tensor, outputs):
+        """Handle subsplit of the layer tensor."""
+        subsplit_dimensions = self._get_subsplit_dimensions(outputs)
+        splitter = UniversalSplitLayer(num_or_size_splits=subsplit_dimensions, axis=-1)
+        split_tensors_list = splitter.split(layer_tensor)
+
+        index = 0
+        for output in outputs:
+            positional_index = output[1]
+            vertical_index = output[2]
+
+            if positional_index not in self.input_storage:
+                self.input_storage[positional_index] = {}
+
+            if vertical_index not in self.input_storage[positional_index]:
+                self.input_storage[positional_index][vertical_index] = []
+
+            self.input_storage[positional_index][vertical_index].append(split_tensors_list[index])
+            index += 1
+
+    def _store_output(self, layer_tensor, outputs):
+        """Store the output tensor in the input storage."""
+        for output in outputs:
+            positional_index = output[1]
+            vertical_index = output[2]
+
+            if positional_index not in self.input_storage:
+                self.input_storage[positional_index] = {}
+
+            if vertical_index not in self.input_storage[positional_index]:
+                self.input_storage[positional_index][vertical_index] = []
+
+            self.input_storage[positional_index][vertical_index].append(layer_tensor)
+
     @staticmethod
     def _get_subsplit_dimensions(outputs):
+        """Get the dimensions for subsplit from the outputs."""
         subsplit_dimensions = []
         for subsplit in outputs:
-            # get the subsplit value
             subsplit_dimensions.append(subsplit[0])
-
         return subsplit_dimensions
